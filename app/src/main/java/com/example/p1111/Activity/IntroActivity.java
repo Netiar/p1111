e370a2e2ee6c31d56b03a3563ae0086e7e4daf22
Netiar <collins.odinga@student@moringaSchool.com>
2022-08-27 12:02:30 +0300

    add new toast message

--- app/src/main/java/com/example/p1111/Activity/IntroActivity.java
+++ app/src/main/java/com/example/p1111/Activity/IntroActivity.java
@@ -1,18 +1,21 @@
 package com.example.p1111.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.p1111.R;
 
 public class IntroActivity extends AppCompatActivity  {
     boolean islottie = false;
+    Button button;
+
 
 
     @Override
@@ -21,41 +24,33 @@ public class IntroActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_intro);
 
         LottieAnimationView lottie = findViewById(R.id.lottie);
-        lottie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        lottie.setOnClickListener(v -> {
 
-                if(islottie){
-                    lottie.setSpeed(-1);
-                    lottie.playAnimation();
-                    islottie = false;
-                } else{
-                    lottie.setSpeed(1);
-                    lottie.playAnimation();
-                    islottie = true;
-                }
+            if(islottie){
+                lottie.setSpeed(-1);
+                lottie.playAnimation();
+                islottie = false;
+            } else{
+                lottie.setSpeed(1);
+                lottie.playAnimation();
+                islottie = true;
             }
         });
 
 
         Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(IntroActivity.this, MainActivity.class));
-            }
-        });
+        button.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)
+        ));
 
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //calling main activity to load after loading screen
-                startActivity(new Intent(IntroActivity.this,MainActivity.class));
-                finish(); //finish loading screen activity
-            }
+        handler.postDelayed(() -> {
+            //calling main activity to load after loading screen
+            startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            Toast.makeText(IntroActivity.this ,"Welcome", Toast.LENGTH_LONG).show();
+            finish(); //finish loading screen activity
         }, 3500);
     }
 
 
 }
+
